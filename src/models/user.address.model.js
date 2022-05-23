2bbb2779d068b33cd35273681a68bad643af3c22
dinoweblog <93374756+dinoweblog@users.noreply.github.com>
2022-05-23 15:32:45 +0530

    update

--- src/models/user.address.model.js
+++ src/models/user.address.model.js
@@ -5,7 +5,7 @@ const userAddressSchema = new mongoose.Schema(
     userName: { type: String, required: true },
     mobile: { type: Number, required: true },
     pincode: { type: Number, required: true },
-    address: { type: String, required: true },
+    addressField: { type: String, required: true },
     town: { type: String, required: true },
     state: { type: String, required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
