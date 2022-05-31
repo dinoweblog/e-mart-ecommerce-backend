90afb801c5a975c88063a754213a0caa6e4b5b12
dinoweblog <93374756+dinoweblog@users.noreply.github.com>
2022-05-31 20:58:37 +0530

    update wishlist  controller

--- src/controllers/wishlist.controller.js
+++ src/controllers/wishlist.controller.js
@@ -35,6 +35,32 @@ router.get("/items/:id", authenticate, async (req, res) => {
   }
 });
 
+router.get("/items/find/:userId/:id", authenticate, async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const prodId = req.params.id;
+    const user = await User.findById(userId).lean().exec();
+
+    const user_id = user._id;
+
+    const wishlist = await Wishlist.find({ userId: user_id, productId: prodId })
+      .lean()
+      .exec();
+
+    let check;
+
+    if (wishlist.length === 0) {
+      check = "true";
+    } else {
+      check = "false";
+    }
+
+    return res.send(check);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
 router.delete("/items/delete/:id", authenticate, async (req, res) => {
   try {
     const wishlist = await Wishlist.findByIdAndDelete(req.params.id);
